2b780ab2d570925bdc7446b74472394909a8ff70
Ali Saeed <asaeed5637@gmail.com>
2024-11-06 17:27:10 -0800

    commit 1

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//include necessary headers for file handling, dat structures, etc.
+#include <iostream>
+#include <map>
+#include <list>
+#include <array>
+#include <string>
+#include <fstream>
+using namespace std;
+
+//define a function ot simulate valet parking over time
+
+
+void simulate(map<int, array<list<string>, 3>>& garage, int trials);
+//parameters:
+// map of parking garage floors, and num of trials u want it to simulate for (>25)
+
+//define main function
+int main() {
+    //initialize a map to store parking garage levels, and each to be associated with array of lists for arrival time, the cars that are parked, and available parking spaces
+    
+
+};
+
+
