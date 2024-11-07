597bad5b2354905f052927f387443e28ce5312d9
Ali Saeed <asaeed5637@gmail.com>
2024-11-06 17:41:35 -0800

    mac decided to udpate on its own sorry

--- main.cpp
+++ main.cpp
@@ -16,9 +16,19 @@ void simulate(map<int, array<list<string>, 3>>& garage, int trials);
 
 //define main function
 int main() {
+    srand(time(0)); //seed random time
     //initialize a map to store parking garage levels, and each to be associated with array of lists for arrival time, the cars that are parked, and available parking spaces
-    
+    map<int, array<list<string>, 3>> garage;
+    // element 0 in array: list 0 will include cars that have parked on that floor
+    // element 1 in array: list 1 will include arrival times of the cars on the floor
+    // element 2 in array: list 2 will show available spaces on that floor by identification 
 
+    //open an external file and 
+    //ifstream fin("data.txt");
+    //if (!fin) {
+        //cout << "Invalid external file!" << endl;
+        //return 0;
+  //  }
 };
 
 
